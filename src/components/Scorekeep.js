1d1c2ef22ee7b063a28b09dc454c42f8ba23f360
yobany <yobanydesign@gmail.com>
2021-06-03 11:48:32 -0400

    Updated scorekeeping logic.

--- src/components/Scorekeep.js
+++ src/components/Scorekeep.js
@@ -11,342 +11,378 @@ const Scorekeep = () => {
     const [redScore, setRedScore] = useState(10)
     const [yellowScore, setYellowScore] = useState(10)
     const [question, setQuestion] = useState(1)
-    const [quizData, setQuizData] = useState([])
+    const [quizData, setQuizData] = useState('')
     const [quizID, setQuizID] = useState('')
     const [currentTime, setCurrentTime] = useState(0)
 
     useEffect(() => {
         db.collection('quizzes').add({
+            q1: {
+                question: 1,
+                rscore: 10,
+                yscore: 10,
+                r1: 0,
+                r2: 0,
+                r3: 0,
+                r4: 0,
+                r5: 0,
+                y1: 0,
+                y2: 0,
+                y3: 0,
+                y4: 0,
+                y5: 0
+            },
+            q2: {
+                question: 2,
+                rscore: 10,
+                yscore: 10,
+                r1: 0,
+                r2: 0,
+                r3: 0,
+                r4: 0,
+                r5: 0,
+                y1: 0,
+                y2: 0,
+                y3: 0,
+                y4: 0,
+                y5: 0
+            },
+            q3: {
+                question: 3,
+                rscore: 10,
+                yscore: 10,
+                r1: 0,
+                r2: 0,
+                r3: 0,
+                r4: 0,
+                r5: 0,
+                y1: 0,
+                y2: 0,
+                y3: 0,
+                y4: 0,
+                y5: 0
+            },
+            q4: {
+                question: 4,
+                rscore: 10,
+                yscore: 10,
+                r1: 0,
+                r2: 0,
+                r3: 0,
+                r4: 0,
+                r5: 0,
+                y1: 0,
+                y2: 0,
+                y3: 0,
+                y4: 0,
+                y5: 0
+            },
+            q5: {
+                question: 5,
+                rscore: 10,
+                yscore: 10,
+                r1: 0,
+                r2: 0,
+                r3: 0,
+                r4: 0,
+                r5: 0,
+                y1: 0,
+                y2: 0,
+                y3: 0,
+                y4: 0,
+                y5: 0
+            },
+            q6: {
+                question: 6,
+                rscore: 10,
+                yscore: 10,
+                r1: 0,
+                r2: 0,
+                r3: 0,
+                r4: 0,
+                r5: 0,
+                y1: 0,
+                y2: 0,
+                y3: 0,
+                y4: 0,
+                y5: 0
+            },
+            q7: {
+                question: 7,
+                rscore: 10,
+                yscore: 10,
+                r1: 0,
+                r2: 0,
+                r3: 0,
+                r4: 0,
+                r5: 0,
+                y1: 0,
+                y2: 0,
+                y3: 0,
+                y4: 0,
+                y5: 0
+            },
+            q8: {
+                question: 8,
+                rscore: 10,
+                yscore: 10,
+                r1: 0,
+                r2: 0,
+                r3: 0,
+                r4: 0,
+                r5: 0,
+                y1: 0,
+                y2: 0,
+                y3: 0,
+                y4: 0,
+                y5: 0
+            },
+            q9: {
+                question: 9,
+                rscore: 10,
+                yscore: 10,
+                r1: 0,
+                r2: 0,
+                r3: 0,
+                r4: 0,
+                r5: 0,
+                y1: 0,
+                y2: 0,
+                y3: 0,
+                y4: 0,
+                y5: 0
+            },
+            q10: {
+                question: 10,
+                rscore: 10,
+                yscore: 10,
+                r1: 0,
+                r2: 0,
+                r3: 0,
+                r4: 0,
+                r5: 0,
+                y1: 0,
+                y2: 0,
+                y3: 0,
+                y4: 0,
+                y5: 0
+            },
+            q11: {
+                question: 11,
+                rscore: 10,
+                yscore: 10,
+                r1: 0,
+                r2: 0,
+                r3: 0,
+                r4: 0,
+                r5: 0,
+                y1: 0,
+                y2: 0,
+                y3: 0,
+                y4: 0,
+                y5: 0
+            },
+            q12: {
+                question: 12,
+                rscore: 10,
+                yscore: 10,
+                r1: 0,
+                r2: 0,
+                r3: 0,
+                r4: 0,
+                r5: 0,
+                y1: 0,
+                y2: 0,
+                y3: 0,
+                y4: 0,
+                y5: 0
+            },
+            q13: {
+                question: 13,
+                rscore: 10,
+                yscore: 10,
+                r1: 0,
+                r2: 0,
+                r3: 0,
+                r4: 0,
+                r5: 0,
+                y1: 0,
+                y2: 0,
+                y3: 0,
+                y4: 0,
+                y5: 0
+            },
+            q14: {
+                question: 14,
+                rscore: 10,
+                yscore: 10,
+                r1: 0,
+                r2: 0,
+                r3: 0,
+                r4: 0,
+                r5: 0,
+                y1: 0,
+                y2: 0,
+                y3: 0,
+                y4: 0,
+                y5: 0
+            },
+            q15: {
+                question: 15,
+                rscore: 10,
+                yscore: 10,
+                r1: 0,
+                r2: 0,
+                r3: 0,
+                r4: 0,
+                r5: 0,
+                y1: 0,
+                y2: 0,
+                y3: 0,
+                y4: 0,
+                y5: 0
+            },
+            q16: {
+                question: 16,
+                rscore: 10,
+                yscore: 10,
+                r1: 0,
+                r2: 0,
+                r3: 0,
+                r4: 0,
+                r5: 0,
+                y1: 0,
+                y2: 0,
+                y3: 0,
+                y4: 0,
+                y5: 0
+            },
+            q17: {
+                question: 17,
+                rscore: 10,
+                yscore: 10,
+                r1: 0,
+                r2: 0,
+                r3: 0,
+                r4: 0,
+                r5: 0,
+                y1: 0,
+                y2: 0,
+                y3: 0,
+                y4: 0,
+                y5: 0
+            },
+            q18: {
+                question: 18,
+                rscore: 10,
+                yscore: 10,
+                r1: 0,
+                r2: 0,
+                r3: 0,
+                r4: 0,
+                r5: 0,
+                y1: 0,
+                y2: 0,
+                y3: 0,
+                y4: 0,
+                y5: 0
+            },
+            q19: {
+                question: 19,
+                rscore: 10,
+                yscore: 10,
+                r1: 0,
+                r2: 0,
+                r3: 0,
+                r4: 0,
+                r5: 0,
+                y1: 0,
+                y2: 0,
+                y3: 0,
+                y4: 0,
+                y5: 0
+            },
+            q20: {
+                question: 20,
+                rscore: 10,
+                yscore: 10,
+                r1: 0,
+                r2: 0,
+                r3: 0,
+                r4: 0,
+                r5: 0,
+                y1: 0,
+                y2: 0,
+                y3: 0,
+                y4: 0,
+                y5: 0
+            },
             date: new Date()
         })
         .then((docRef) => {
-            console.log(docRef.id)
             setQuizID(docRef.id)
+            return docRef
+        })
+        .then((docRef) => {
+            console.log(docRef.id)
         })
     }, [])
 
-    let obj = [
-        {
-            question: 1,
-            rscore: `${redScore}`,
-            yscore: `${yellowScore}`,
-            r1: 0,
-            r2: 0,
-            r3: 0,
-            r4: 0,
-            r5: 0,
-            y1: 0,
-            y2: 0,
-            y3: 0,
-            y4: 0,
-            y5: 0,
-        },{
-            question: 2,
-            rscore: `${redScore}`,
-            yscore: `${yellowScore}`,
-            r1: 0,
-            r2: 0,
-            r3: 0,
-            r4: 0,
-            r5: 0,
-            y1: 0,
-            y2: 0,
-            y3: 0,
-            y4: 0,
-            y5: 0,
-        },{
-            question: 3,
-            rscore: `${redScore}`,
-            yscore: `${yellowScore}`,
-            r1: 0,
-            r2: 0,
-            r3: 0,
-            r4: 0,
-            r5: 0,
-            y1: 0,
-            y2: 0,
-            y3: 0,
-            y4: 0,
-            y5: 0,
-        },{
-            question: 4,
-            rscore: `${redScore}`,
-            yscore: `${yellowScore}`,
-            r1: 0,
-            r2: 0,
-            r3: 0,
-            r4: 0,
-            r5: 0,
-            y1: 0,
-            y2: 0,
-            y3: 0,
-            y4: 0,
-            y5: 0,
-        },{
-            question: 5,
-            rscore: `${redScore}`,
-            yscore: `${yellowScore}`,
-            r1: 0,
-            r2: 0,
-            r3: 0,
-            r4: 0,
-            r5: 0,
-            y1: 0,
-            y2: 0,
-            y3: 0,
-            y4: 0,
-            y5: 0,
-        },{
-            question: 6,
-            rscore: `${redScore}`,
-            yscore: `${yellowScore}`,
-            r1: 0,
-            r2: 0,
-            r3: 0,
-            r4: 0,
-            r5: 0,
-            y1: 0,
-            y2: 0,
-            y3: 0,
-            y4: 0,
-            y5: 0,
-        },{
-            question: 7,
-            rscore: `${redScore}`,
-            yscore: `${yellowScore}`,
-            r1: 0,
-            r2: 0,
-            r3: 0,
-            r4: 0,
-            r5: 0,
-            y1: 0,
-            y2: 0,
-            y3: 0,
-            y4: 0,
-            y5: 0,
-        },{
-            question: 8,
-            rscore: `${redScore}`,
-            yscore: `${yellowScore}`,
-            r1: 0,
-            r2: 0,
-            r3: 0,
-            r4: 0,
-            r5: 0,
-            y1: 0,
-            y2: 0,
-            y3: 0,
-            y4: 0,
-            y5: 0,
-        },{
-            question: 9,
-            rscore: `${redScore}`,
-            yscore: `${yellowScore}`,
-            r1: 0,
-            r2: 0,
-            r3: 0,
-            r4: 0,
-            r5: 0,
-            y1: 0,
-            y2: 0,
-            y3: 0,
-            y4: 0,
-            y5: 0,
-        },{
-            question: 10,
-            rscore: `${redScore}`,
-            yscore: `${yellowScore}`,
-            r1: 0,
-            r2: 0,
-            r3: 0,
-            r4: 0,
-            r5: 0,
-            y1: 0,
-            y2: 0,
-            y3: 0,
-            y4: 0,
-            y5: 0,
-        },{
-            question: 11,
-            rscore: `${redScore}`,
-            yscore: `${yellowScore}`,
-            r1: 0,
-            r2: 0,
-            r3: 0,
-            r4: 0,
-            r5: 0,
-            y1: 0,
-            y2: 0,
-            y3: 0,
-            y4: 0,
-            y5: 0,
-        },{
-            question: 12,
-            rscore: `${redScore}`,
-            yscore: `${yellowScore}`,
-            r1: 0,
-            r2: 0,
-            r3: 0,
-            r4: 0,
-            r5: 0,
-            y1: 0,
-            y2: 0,
-            y3: 0,
-            y4: 0,
-            y5: 0,
-        },{
-            question: 13,
-            rscore: `${redScore}`,
-            yscore: `${yellowScore}`,
-            r1: 0,
-            r2: 0,
-            r3: 0,
-            r4: 0,
-            r5: 0,
-            y1: 0,
-            y2: 0,
-            y3: 0,
-            y4: 0,
-            y5: 0,
-        },{
-            question: 14,
-            rscore: `${redScore}`,
-            yscore: `${yellowScore}`,
-            r1: 0,
-            r2: 0,
-            r3: 0,
-            r4: 0,
-            r5: 0,
-            y1: 0,
-            y2: 0,
-            y3: 0,
-            y4: 0,
-            y5: 0,
-        },{
-            question: 15,
-            rscore: `${redScore}`,
-            yscore: `${yellowScore}`,
-            r1: 0,
-            r2: 0,
-            r3: 0,
-            r4: 0,
-            r5: 0,
-            y1: 0,
-            y2: 0,
-            y3: 0,
-            y4: 0,
-            y5: 0,
-        },{
-            question: 16,
-            rscore: `${redScore}`,
-            yscore: `${yellowScore}`,
-            r1: 0,
-            r2: 0,
-            r3: 0,
-            r4: 0,
-            r5: 0,
-            y1: 0,
-            y2: 0,
-            y3: 0,
-            y4: 0,
-            y5: 0,
-        },{
-            question: 17,
-            rscore: `${redScore}`,
-            yscore: `${yellowScore}`,
-            r1: 0,
-            r2: 0,
-            r3: 0,
-            r4: 0,
-            r5: 0,
-            y1: 0,
-            y2: 0,
-            y3: 0,
-            y4: 0,
-            y5: 0,
-        },{
-            question: 18,
-            rscore: `${redScore}`,
-            yscore: `${yellowScore}`,
-            r1: 0,
-            r2: 0,
-            r3: 0,
-            r4: 0,
-            r5: 0,
-            y1: 0,
-            y2: 0,
-            y3: 0,
-            y4: 0,
-            y5: 0,
-        },{
-            question: 19,
-            rscore: `${redScore}`,
-            yscore: `${yellowScore}`,
-            r1: 0,
-            r2: 0,
-            r3: 0,
-            r4: 0,
-            r5: 0,
-            y1: 0,
-            y2: 0,
-            y3: 0,
-            y4: 0,
-            y5: 0,
-        },{
-            question: 20,
-            rscore: `${redScore}`,
-            yscore: `${yellowScore}`,
-            r1: 0,
-            r2: 0,
-            r3: 0,
-            r4: 0,
-            r5: 0,
-            y1: 0,
-            y2: 0,
-            y3: 0,
-            y4: 0,
-            y5: 0,
+    useEffect(() => {
+        if (quizID != '') {
+            db.collection('quizzes').doc(quizID).get()
+            .then((docRef) => {
+                setQuizData(docRef.data())
+                console.log(`Setting quiz data to:`, docRef.data())
+            })
         }
-    ]
+    }, [quizID])
 
     const renderScoreTable = () => {
-        return obj.map((item) => {
-          return (<tr>
-            <td>{item.question}</td>
-            <td>{item.r1}</td>
-            <td>{item.r2}</td>
-            <td>{item.r3}</td>
-            <td>{item.r4}</td>
-            <td>{item.r5}</td>
-            <td>{item.rscore}</td>
-            <td>{item.yscore}</td>
-            <td>{item.y1}</td>
-            <td>{item.y2}</td>
-            <td>{item.y3}</td>
-            <td>{item.y4}</td>
-            <td>{item.y5}</td>
+        if (quizData != '') {
+            let quizDataArr = Object.values(quizData)
+            console.log(quizDataArr.sort())
+            return quizDataArr.map((item) => {
+                return <tr>
+                            <td>{item.question}</td>
+                            <td>{item.r1}</td>
+                            <td>{item.r2}</td>
+                            <td>{item.r3}</td>
+                            <td>{item.r4}</td>
+                            <td>{item.r5}</td>
+                            <td>{item.rscore}</td>
+                            <td>{item.yscore}</td>
+                            <td>{item.y1}</td>
+                            <td>{item.y2}</td>
+                            <td>{item.y3}</td>
+                            <td>{item.y4}</td>
+                            <td>{item.y5}</td>
+                        </tr>
+            })
+        }
+        return (<tr>
+            <td>1</td>
+            <td>0</td>
+            <td>0</td>
+            <td>0</td>
+            <td>0</td>
+            <td>0</td>
+            <td>10</td>
+            <td>10</td>
+            <td>0</td>
+            <td>0</td>
+            <td>0</td>
+            <td>0</td>
+            <td>0</td>
           </tr>)
-        })
       }
     
     const handleQuizzerAction = value => {
         if(value === 'R1-C') {
             setRedScore(redScore + questionValue(question))
             setQuestion(q => q + 1)
-            db.collection("quizzes").doc(quizID).collection('quizTable').doc(`question${question}`).set({
-                question: question,
-                    rscore: redScore,
-                    yscore: yellowScore,
-                    r1: 0,
-                    r2: 0,
-                    r3: 0,
-                    r4: 0,
-                    r5: 0,
-                    y1: 0,
-                    y2: 0,
-                    y3: 0,
-                    y4: 0,
-                    y5: 0,
+            db.collection("quizzes").doc(quizID).update({
+                "q1.r1": (questionValue(question))
             })
             .then((docRef) => {
                 console.log(`Success!`)
